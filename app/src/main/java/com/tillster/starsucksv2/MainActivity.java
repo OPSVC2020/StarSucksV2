fdf8601bed11716a782db7a9c0adcab2561bf9fe
TillCM <sarinacm@gmail.com>
2020-08-03 13:10:40 +0200

    Initial commit

---
+++ app/src/main/java/com/tillster/starsucksv2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.tillster.starsucksv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
